84130104307f8ca47c025999e4446d113aff7a4b
翁天俊 <justinwuny@163.com>
2019-12-11 17:45:36 +0800

    CORS fixed

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const mysql = require('mysql');
-const { MYSQL_POOL_OPTIONS } = require('./config');
+const { MYSQL_POOL_OPTIONS, DEBUG } = require('./config');
 
 let pool = mysql.createPool(MYSQL_POOL_OPTIONS);
 const indexRouter = require('./routes/index');
@@ -24,8 +24,12 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(function (req, res, next) {
-  req.mysql = pool;
-  next();
+    req.mysql = pool;
+    // CORS debugging
+    if (DEBUG) {
+        res.setHeader("Access-Control-Allow-Origin", "http://127.0.0.1:8080")
+    }
+    next();
 });
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
@@ -33,18 +37,18 @@ app.use('/message', messageRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
-  next(createError(404));
+    next(createError(404));
 });
 
 // error handler
 app.use(function (err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
 });
 
 module.exports = app;
